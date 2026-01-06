1af364df6fe0c4ef6ae878567447f0cbcd5474f8
kamlesh1805 <a.kamlesh2005@gmail.com>
2026-01-06 16:19:46 +0530

    Create 1161. Maximum Level Sum of Binary Tree.c

---
+++ 1161. Maximum Level Sum of Binary Tree.c
@@ -0,0 +1,59 @@
+/*Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+Example 1:
+
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+Example 2:
+
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-105 <= Node.val <= 105*/
+
+int maxLevelSum(struct TreeNode* root) 
+{
+    struct TreeNode* queue[10000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    int level = 1, ans = 1;
+    long long maxSum = -1000000000000000000LL;
+
+    while (front < rear) {
+        int size = rear - front;
+        long long sum = 0;
+
+        for (int i = 0; i < size; i++) {
+            struct TreeNode* node = queue[front++];
+            sum += node->val;
+
+            if (node->left)
+                queue[rear++] = node->left;
+            if (node->right)
+                queue[rear++] = node->right;
+        }
+
+        if (sum > maxSum) {
+            maxSum = sum;
+            ans = level;
+        }
+
+        level++;
+    }
+
+    return ans;
+}
+
+
